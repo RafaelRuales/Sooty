3e82d066bae169c245878950577a2aaa1cb855c2
rafaelruales <kira.advisors@gmail.com>
2020-08-12 20:37:30 -0400

    Finished secondwrite url call and added color to output if malicious url is detected

--- Sooty.py
+++ Sooty.py
@@ -9,6 +9,7 @@
 import base64
 import hashlib
 import html.parser
+import os
 import re
 import json
 import time
@@ -405,19 +406,21 @@ def result_sw(api_endpoint, api_key, uuid):
                                         'api_key': api_key,
                                         'format': 'json'})
     result = response.json()
-    print(result['result'])
+    print("-" * 16, "SECONDWRITE SCAN", "-" * 16, sep="\n")
+    print_results(result['result'])
+
     if result['malware_classification']:
-        print(result['malware_classification'])
-
-
-
-
-
+        print('Malware Classification: ')
+        print('\033[1;31m- Confidence: {} \033[00m'.format(result['malware_classification'][0]['confidence']))
+        print('\033[1;31m- Description: {} \033[00m'.format(result['malware_classification'][0]['description']))
 
 
 def print_results(output):
     for key, value in output.items():
-        if key.lower() == 'malicious' and value > 0 or key.lower() == 'malicious' and value == 'True':
+        if key.lower() == 'malicious' and value > 0 or \
+                key.lower() == 'malicious' and value == 'True' or \
+                key.lower() == 'malware_confidence' and value == 100 or \
+                key.lower() == 'result' and value == 'Malicious':
             key = "\033[1;31m{}\033[00m".format(key)
             value = "\033[1;31m{}\033[00m".format(value)
         print(key + ":", value)
@@ -442,38 +445,11 @@ def b64Decoder():
         print(' No Base64 Encoded String Found')
     decoderMenu()
 
-# def cisco7Decoder():
-#     pw = input(' Enter Cisco Password 7: ').strip()
-#
-#     key = [0x64, 0x73, 0x66, 0x64, 0x3b, 0x6b, 0x66, 0x6f, 0x41,
-#     0x2c, 0x2e, 0x69, 0x79, 0x65, 0x77, 0x72, 0x6b, 0x6c,
-#     0x64, 0x4a, 0x4b, 0x44, 0x48, 0x53, 0x55, 0x42]
-#
-#     try:
-#         # the first 2 characters of the password are the starting index in the key array
-#         index = int(pw[:2],16)
-#
-#         # the remaining values are the characters in the password, as hex bytes
-#         pw_text = pw[2:]
-#         pw_hex_values = [pw_text[start:start+2] for start in range(0,len(pw_text),2)]
-#
-#         # XOR those values against the key values, starting at the index, and convert to ASCII
-#         pw_chars = [chr(key[index+i] ^ int(pw_hex_values[i],16)) for i in range(0,len(pw_hex_values))]
-#
-#         pw_plaintext = ''.join(pw_chars)
-#         print("Password: " + pw_plaintext)
-#
-#     except Exception as e:
-#         print(e)
-#
-#     decoderMenu()
-
 
 def repChecker():
     print('\n' + '-' * 35, ' R E P U T A T I O N     C H E C K ', '-' * 35, sep='\n')
     rawInput = input("Enter IP, URL or Email Address: ").split()
     ip = str(rawInput[0])
-
     s = re.findall(r'\S+@\S+', ip)
     if s:
         print(' Email Detected...')
@@ -481,40 +457,6 @@ def repChecker():
     else:
         whoIsPrint(ip)
         wIP = socket.gethostbyname(ip)
-
-        print("\n VirusTotal Report:")
-        url = 'https://www.virustotal.com/vtapi/v2/ip-address/report'
-        params = {'apikey': configvars.data['VT_API_KEY'], 'ip': wIP}
-        response = requests.get(url, params=params)
-
-        pos = 0
-        tot = 0
-        if response.status_code == 200:
-            try:    # try IP else fall through to URL
-                result = response.json()
-                for each in result['detected_urls']:
-                    tot = tot + 1
-                    pos = pos + each['positives']
-
-                if tot != 0:
-                    print("   No of Reportings: " + str(tot))
-                    print("   Average Score:    " + str(pos / tot))
-                    print("   VirusTotal Report Link: " + "https://www.virustotal.com/gui/ip-address/" + str(ip))
-                else:
-                    print("   No of Reportings: " + str(tot))
-            except:
-                try: #EAFP
-                    url = 'https://www.virustotal.com/vtapi/v2/url/report'
-                    params = {'apikey': configvars.data['VT_API_KEY'], 'resource': wIP}
-                    response = requests.get(url, params=params)
-                    result = response.json()
-                    print("\n VirusTotal Report:")
-                    print("   URL Malicious Reportings: " + str(result['positives']) + "/" + str(result['total']))
-                    print("   VirusTotal Report Link: " + str(result['permalink']))  # gives URL for report (further info)
-                except:
-                    print(" Not found in database")
-        else:
-            print(" There's been an error - check your API key, or VirusTotal is possible down")
 
         try:
             TOR_URL = "https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1"
